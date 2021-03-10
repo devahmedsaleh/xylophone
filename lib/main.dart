624d9ce8fb9ff179b8dd9f41cf2034d1bb55460a
Ahmed Saleh <>
2021-03-10 15:41:24 +0200

    add sample buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,28 @@ class Xylophone extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {},
+                child: Container(),
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Container(),
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Container(),
+                color: Colors.yellow,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
