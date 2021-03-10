63e0c7236b07f2a0f51fab5999fa7234639847d5
Ahmed Saleh <>
2021-03-10 14:58:38 +0200

    initialize material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Xylophone());
+}
+
+class Xylophone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
